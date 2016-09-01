937a06db3fbe26c70adb97568a730f4d42fc12a3
Roy Segall <roy.segall@gizra.com>
2016-09-01 15:23:10 +0300

    Adding scrolling.

--- src/js/main.js
+++ src/js/main.js
@@ -34,5 +34,18 @@ $(document).ready(function() {
   $(".remove").click(function() {
     $(".session-info").removeClass('pop');
   });
+
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top - 60
+        }, 1000);
+        return false;
+      }
+    }
+  });
   
 });
